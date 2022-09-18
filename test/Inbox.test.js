2bb5bd0428cc9bebcdc3ad6fd20f91bdd6d14f92
thasup <thanachonfirst@hotmail.com>
2022-09-18 18:25:42 +0700

    Deployment with web3

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,20 +1,31 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
+const { interface, bytecode } = require('../compile');
 
 const web3 = new Web3(ganache.provider());
 
 let fetchAccounts;
+let inbox;
 
 beforeEach(async () => {
   // Get a list of all accounts
   fetchAccounts = await web3.eth.getAccounts();
 
   //Use one of those accounts to deploy the contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: ['Hi there!'],
+    })
+    .send({
+      from: fetchAccounts[0],
+      gas: '1000000',
+    })
 });
 
 describe('Inbox', () => {
   it('deploy a contract', () => {
-    console.log(fetchAccounts);
+    console.log(inbox);
   });
 });
